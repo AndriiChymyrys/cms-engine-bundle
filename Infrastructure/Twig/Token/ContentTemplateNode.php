123ca0128c2b89113f775403a491ac64b8cf073a
Andrii Chymyrys <andrii.chymyrys@gmail.com>
2022-12-09 12:41:12 +0200

    [CMS Engine] improvement

--- Infrastructure/Twig/Token/ContentTemplateNode.php
+++ Infrastructure/Twig/Token/ContentTemplateNode.php
@@ -19,16 +19,12 @@ class ContentTemplateNode extends Node
         $name = str_replace('.', DIRECTORY_SEPARATOR, $this->getAttribute('name'));
 
         $compiler
-            ->write('$themeName = $context["render"]->getThemeProvider()->getName();')
-            ->raw(PHP_EOL)
-            ->write('$templateType = \WideMorph\Cms\Bundle\CmsEngineBundle\Domain\Theme\TemplatePathResolverInterface::THEME_CONTENT_TEMPLATE_TYPE;')
-            ->raw(PHP_EOL)
             ->write('$this->loadTemplate(')
             ->raw('$context["render"]->getPathResolver()->getFullPublicFileName(')
             ->raw(PHP_EOL)
-            ->raw('$themeName, ')
+            ->raw('$context["render"]->getThemeProvider()->getName(), ')
             ->raw(PHP_EOL)
-            ->raw('$templateType, ')
+            ->raw('\WideMorph\Cms\Bundle\CmsEngineBundle\Domain\Theme\TemplatePathResolverInterface::THEME_CONTENT_TEMPLATE_TYPE, ')
             ->raw(PHP_EOL)
             ->raw(sprintf('"%s", ', $name))
             ->raw(PHP_EOL)
@@ -39,10 +35,7 @@ class ContentTemplateNode extends Node
             ->raw(PHP_EOL)
             ->repr($this->getTemplateLine())
             ->raw(')')
-            ->raw('->display(')
-            ->raw('twig_array_merge($context,')
-            ->raw('["name" => "value"])') // need to generate from contentTemplate fields
-            ->raw(');')
+            ->raw('->display($context);')
         ;
     }
 }
